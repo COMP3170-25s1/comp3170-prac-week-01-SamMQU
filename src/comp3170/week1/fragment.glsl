7925e7b95d3b2b428c254b7039b2ead68f2f1d7c
SamMQU <160988408+SamMQU@users.noreply.github.com>
2025-03-12 11:04:37 +1100

    week 1 diamond completed

--- src/comp3170/week1/fragment.glsl
+++ src/comp3170/week1/fragment.glsl
@@ -7,9 +7,9 @@ layout(location = 0) out vec4 o_colour;	// output to colour buffer
 
 void main() {
    vec2 p = gl_FragCoord.xy / u_screenSize;   // scale p into range (0,0) to (1,1)
-   float d = distance(p, vec2(0.5, 0.5));     // calculate distance to midpoint
+   vec2 center = vec2(0.5,0.5);
   
-   if (d < 0.5) {
+   if (abs(p.x - center.x) + abs(p.y - center.y) < 0.4) {
       o_colour = vec4(u_colour, 1);
    }
    else {
